a14ca9fc3a38fbb725e286fa626bf161876b6c28
hbc <bcxxxxxx@gmail.com>
2014-10-08 08:47:07 +0800

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# gists-as-tricks
+
+Gists as tricks!
+
+## Running
+
+To start a web server for the application, run:
+
+    lein ring server
---
+++ project.clj
@@ -0,0 +1,14 @@
+(defproject gists-as-tricks "0.0.1-SNAPSHOT"
+  :description "Gists as tricks!"
+  :url "http://tricks.youknowmymind.com"
+  :min-lein-version "2.0.0"
+
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [compojure "1.1.9"]]
+
+  :plugins [[lein-ring "0.8.12"]]
+  :ring {:handler gists-as-tricks.handler/app}
+
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring-mock "0.1.5"]]}})
